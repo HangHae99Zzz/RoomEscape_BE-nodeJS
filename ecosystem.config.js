3dceb191634986254907070b579da488fc531ff6
paran21 <knospe1@naver.com>
2022-03-23 23:17:04 +0900

    Feat: pm2 사용을 위한 설정 파일 추가

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [
+    {
+      "name": "zzz-roomescape",
+      "script": "./node_modules/.bin/ts-node",
+      "args": "./src/index.ts",
+      "watch": true
+  }
+],
+
+  
+};
