ec6756a9948be8331534eebb6089a2d8811bd3f8
paran21 <knospe1@naver.com>
2022-04-06 11:14:24 +0900

    Comment: 사용하지 않는 기능 주석 삭제

--- app.js
+++ app.js
@@ -8,18 +8,4 @@ app.use(cors());
 
 app.set("port", process.env.PORT || 3000);
 
-// //Request 로그 남기는 미들웨어 
-// const requestMiddleware = (req, res, next) => {
-//    console.log( "Request URL:", req.originalUrl, " - ", 
-//    new Date(+new Date() + 3240 * 10000) 
-//    .toISOString() 
-//    .replace("T", " ") 
-//    .replace(/\..*/, "") 
-//    ); 
-//    next(); 
-//   };
-
-// // request log
-// app.use(requestMiddleware); 
-
 module.exports = app;
